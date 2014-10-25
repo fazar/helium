935f77633895bc66115a6c578f6148269e76967b
Mochamad Fazar <mochamad.fazar@gmail.com>
2014-10-25 16:39:48 +0700

    check if options left empty

--- decode/modules/shortcodes/class.bootstrapper.php
+++ decode/modules/shortcodes/class.bootstrapper.php
@@ -1,5 +1,5 @@
 <?php
-	class DC_shortcodes_bootstrapper extends DC_boostrapper{
+	class DC_shortcodes_bootstrapper extends DC_bootstrapper{
 		function __construct(){
 			$this->instantiate('audio');
 			$this->instantiate('video');
